eb3088164bf8fc1574fa23d039372163638b7289
YehYungCheng <yehyungcheng@YungChengtekiMacBook-Air.local>
2017-03-18 15:38:05 +0800

    Initial Commit

---
+++ TouchBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TouchBar
+//
+//  Created by YehYungCheng on 2017/3/18.
+//  Copyright © 2017年 YehYungCheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
