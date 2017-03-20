b0a20f5693f13648b264fae3befb427c06b4da18
Wayne Yeh <wayne.yeh@eten.com.tw>
2017-03-20 15:08:39 +0800

    change knob to static object

--- TouchBar/ToolbarSlider.swift
+++ TouchBar/ToolbarSlider.swift
@@ -10,14 +10,14 @@ import Cocoa
 
 private class ToolbarSliderCell: NSSliderCell {
 	
-	class var knob: NSImage {
+	static var knob: NSImage = {
 		let frame = NSRect(x: 0, y: 0, width: 32, height: 32)
 		
 		let image = NSImage(size: frame.size)
 		image.lockFocus()
 		
 		// draw a rounded Rectangle
-		let path = NSBezierPath(roundedRect: frame, xRadius: 8, yRadius: 8)
+		let path = NSBezierPath(roundedRect: frame, xRadius: 8, yRadius: 12)
 		NSColor.lightGray.set()
 		path.fill()
 		
@@ -28,10 +28,10 @@ private class ToolbarSliderCell: NSSliderCell {
 		
 		image.unlockFocus()
 		return image
-	}
+	}()
 	
 	override func drawKnob(_ knobRect: NSRect) {
-		ToolbarSliderCell.knob.draw(in: knobRect.insetBy(dx: 0, dy: 6))
+		ToolbarSliderCell.knob.draw(in: knobRect.insetBy(dx: 0, dy: 6.5))
 	}
 
 }
